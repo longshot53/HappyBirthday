53cf77b03cc867f064364a72e00f8841a16a5b7c
longshot53 <collin.belcer@mavs.uta.edu>
2014-09-17 08:22:27 -0700

    first real upload

---
+++ app/src/main/java/podly/happybirthday/MyActivity.java
@@ -0,0 +1,83 @@
+package podly.happybirthday;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
+
+
+
+
+public class MyActivity extends Activity {
+
+    MediaPlayer ourSong;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        ourSong = MediaPlayer.create(MyActivity.this, R.raw.happy_birthday_music);
+        ourSong.start();
+        blinkText();
+    }
+
+    private void blinkText() {
+        // TODO Auto-generated method stub
+        final Handler handler = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int timeToBlink = 500;    //in ms
+                try{
+                    Thread.sleep(timeToBlink);
+                }catch (Exception e) {
+
+                }
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView txt = (TextView) findViewById(R.id.Greeting);
+                        if(txt.getVisibility() == View.VISIBLE){
+                            txt.setVisibility(View.INVISIBLE);
+                        }else{
+                            txt.setVisibility(View.VISIBLE);
+                        }
+                        blinkText();
+                    }
+                });
+            }}).start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause(){
+        // TODO Auto-generated method stub
+        super.onPause();
+        ourSong.release();
+        finish();
+    }
+}
